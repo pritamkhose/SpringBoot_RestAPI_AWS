03251fb1e9ef0c03c526de06f8f950cc42ab7c3e
Pritam <pritamkhose@gmail.com>
2018-10-08 18:45:47 +0530

    v0.1

---
+++ sqlquery.sql
@@ -0,0 +1,14 @@
+SELECT * FROM sprinexamples.articles;
+
+-- Dumping structure for table concretepage.articles
+CREATE TABLE IF NOT EXISTS `articles` (
+  `article_id` int(5) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+-- Dumping data for table concretepage.articles: ~3 rows (approximately)
+INSERT INTO `articles` (`article_id`, `title`, `category`) VALUES
+	(1, 'Java Concurrency', 'Java'),
+	(2, 'Hibernate HQL ', 'Hibernate'),
+	(3, 'Spring MVC with Hibernate', 'Spring'); 
